791d31e5b3270aac4e4c2f45a964d4830ef74a93
Basila123 <basilanoushad92@gmail.com>
2022-08-02 01:45:13 +0530

    added the react dashboard project

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'; 
+import Logo from '../../imgs/logo.png';
+import './Sidebar.css';
+import {SidebarData} from '../../Data/Data';
+import { UilSignOutAlt } from "@iconscout/react-unicons";
+import { UilBars } from "@iconscout/react-unicons";
+import { motion } from "framer-motion";
+import { useState } from 'react';
+const Sidebar = () => {
+
+  const [selected, setSelected] = useState(0);
+
+
+  return (
+    <div className='Sidebar'>
+      {/*logo */}
+      <div className='logo'>
+        <img src={Logo}  alt=''/>
+        <span>
+          Sh<span>o</span>ps 
+        </span>
+      </div>
+      
+      {/*menu items */}
+      <div className='Menu'>
+
+      {SidebarData.map((item, index) => {
+          return (
+            <div className={selected === index ? "menuItem active" : "menuItem"}
+              key={index}
+              onClick={() => setSelected(index)}>
+              <item.icon />
+              <span>{item.heading}</span>
+            </div>
+          );
+        })}
+        <div className='menuItem'>
+        <UilSignOutAlt/>
+
+        </div>
+         </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
